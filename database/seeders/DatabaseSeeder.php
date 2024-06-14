96494b886955791f8193fabdfdd9ef36f226c40d
smtbos <smtbos@gmail.com>
2024-06-14 22:29:28 +0530

    Add HasRoles Trait to User Model, Implement Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $permissins = [
+            'note-create',
+            'note-read',
+            'note-update',
+            'note-delete',
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        foreach ($permissins as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $adminRole = Role::create(['name' => 'Admin']);
+
+        $adminRole->givePermissionTo($permissins);
+
+        $userRole = Role::create(['name' => 'User']);
+
+        $userRole->givePermissionTo([
+            'note-create',
+            'note-read',
+            'note-update',
         ]);
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $admin->assignRole('Admin');
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $user->assignRole('User');
     }
 }
